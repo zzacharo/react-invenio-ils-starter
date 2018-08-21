eece96f49c969a652b8faebecb5918de44476e4c
ioanungurean <inungurean@gmail.com>
2018-08-21 14:34:59 +0200

    Add BookList feature

---
+++ src/components/BookList/BookList.mock.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    title: 'Lorem ipsum d.',
+    description:
+      'Amet aut aperiam reiciendis error, nemo maxime unde facere harum.',
+  },
+  {
+    id: 2,
+    title: 'Quo eligendi qu!',
+    description:
+      'Vero maxime saepe eos fuga impedit animi, officiis blanditiis assumenda.',
+  },
+  {
+    id: 3,
+    title: 'Consequuntur quaerat',
+    description:
+      'Mollitia, maiores explicabo nam adipisci repudiandae eligendi eveniet.',
+  },
+  {
+    id: 4,
+    title: 'Suscipit at aliquam!',
+    description:
+      'Facilis sunt reiciendis cum placeat deleniti earum unde quo sit!',
+  },
+  {
+    id: 5,
+    title: 'Illum odio numquam?',
+    description:
+      'Soluta dolorum quo voluptate dolor ex quia aliquam expedita rerum!',
+  },
+  {
+    id: 6,
+    title: 'Est odio sunt?',
+    description:
+      'Doloribus dolorum ratione eligendi laudantium voluptatibus sunt.',
+  },
+  {
+    id: 7,
+    title: 'Esse praesentium s!',
+    description:
+      'Modi consequatur suscipit, dolorum nihil esse necessitatibus et sed atque!',
+  },
+  // {
+  //   id: 8,
+  //   title: 'Voluptatibus commodi?',
+  //   description:
+  //     'Earum iusto doloremque ad praesentium ut mollitia, quae libero.',
+  // },
+  // {
+  //   id: 9,
+  //   title: 'Voluptatem possimus ve.',
+  //   description:
+  //     'Neque voluptatem cum asperiores pariatur, sit dignissimos rerum?',
+  // },
+  // {
+  //   id: 10,
+  //   title: 'Maiores facilis es.',
+  //   description:
+  //     'Sint, excepturi! Reprehenderit quo animi maxime et nemo ipsum magni?',
+  // },
+];
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+import './Footer.css';
+
+export default class Footer extends Component {
+  render() {
+    return <Menu>Footer</Menu>;
+  }
+}
