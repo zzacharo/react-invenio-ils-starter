016738337a1b8a4f747bbd0013780e5e969f85be
ioanungurean <inungurean@gmail.com>
2018-08-23 16:36:37 +0200

    Add book details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Segment } from 'semantic-ui-react';
 import Home from './pages/Home';
+import BookDetails from './pages/BookDetails';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -17,6 +18,7 @@ export default class App extends Component {
           <div className="app-content">
             <Header />
             <Route exact path="/" component={Home} />
+            <Route path="/record/:recid" component={BookDetails} />
             <Footer />
           </div>
         </Segment>
