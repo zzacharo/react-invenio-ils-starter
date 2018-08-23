82f0790caf53a7942485666170a855d7fb7406dd
ioanungurean <inungurean@gmail.com>
2018-08-23 18:24:10 +0200

    Add link to the homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Segment } from 'semantic-ui-react';
 import Home from './pages/Home';
 import BookDetails from './pages/BookDetails';
 import Header from './components/Header';
@@ -14,14 +13,14 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        <Segment className="app">
+        <div className="app">
           <div className="app-content">
             <Header />
             <Route exact path="/" component={Home} />
             <Route path="/record/:recid" component={BookDetails} />
             <Footer />
           </div>
-        </Segment>
+        </div>
       </Router>
     );
   }
